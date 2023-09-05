d95841278058990a89349ca459529b6a45590e32
hab-ee <130477167+hab-ee@users.noreply.github.com>
2023-09-04 22:44:02 -0700

    Add files via upload

---
+++ bubble sort.c
@@ -0,0 +1,27 @@
+
+void bubble_sort(int arr[], int n) {  
+  int i, j;  
+  for (i = 0; i < n - 1; i++) {  
+    for (j = 0; j < n - i - 1; j++) {  
+      if (arr[j] > arr[j + 1]) {  
+        int temp = arr[j];  
+        arr[j] = arr[j + 1];  
+        arr[j + 1] = temp;  
+      }  
+    }  
+  }  
+}  
+int main()
+{
+	int i;
+  int arr[] = {64, 34, 25, 12, 22, 11, 90};  
+  int n = sizeof(arr) / sizeof(arr[0]);  
+  bubble_sort(arr, n);  
+  printf("Sorted array: "); 
+  scanf("%d",&n);
+  for(i = 0; i <n; i++)
+  {
+    printf("%d ", arr[i]);  
+  }  
+  return 0; 
+}
